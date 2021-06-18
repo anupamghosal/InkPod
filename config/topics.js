853a1960030dcb525ae6d2889dc70eb1d0f5132e
SumanX22 <suman.mesaha22@gmail.com>
2021-06-19 05:16:45 +0530

    added images

--- config/topics.js
+++ config/topics.js
@@ -4,54 +4,64 @@ const topics = [
   {
     title: "Current Affairs",
     value: "current-affairs",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/current.jpg`,
   },
   {
     title: "Politics",
     value: "politics",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/politics.jpg`,
   },
   {
     title: "Sports",
     value: "sports",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/sports.jpg`,
   },
   {
     title: "Technology",
     value: "technology",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/technology.jpg`,
   },
   {
     title: "Business",
     value: "business",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/business.jpg`,
   },
   {
     title: "Entertainment",
     value: "entertainment",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/entertainment.jpg`,
   },
   {
     title: "International",
     title: "international",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/international.jpg`,
   },
   {
     title: "Automobile",
     value: "automobile",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    imgUrl: `${BASE_URL}/topics/automobile.jpg`,
   },
   {
     title: "Fashion",
-    value: "Fashion",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    value: "fashion",
+    imgUrl: `${BASE_URL}/topics/fashion.jpg`,
   },
   {
     title: "Health & Lifestyle",
-    imgUrl: `${BASE_URL}/topics/topicDefault.png`,
+    value: "health",
+
+    imgUrl: `${BASE_URL}/topics/health.jpg`,
   },
-  { title: "Travel", imgUrl: `${BASE_URL}/topics/topicDefault.png` },
-  { title: "Miscellaneous", imgUrl: `${BASE_URL}/topics/topicDefault.png` },
+  {
+    title: "Travel",
+    value: "travel",
+    imgUrl: `${BASE_URL}/topics/travel.jpg`,
+  },
+  {
+    title: "Miscellaneous",
+    value: "miscellaneous",
+    imgUrl: `${BASE_URL}/topics/miscellaneous.jpg`,
+  },
 ];
 
 module.exports = topics;
